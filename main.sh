3d9f8dc2d4d2192403fdd264c1bcb62f851d0ff1
RadSan7 <radek.san7@icloud.com>
2025-08-16 14:29:53 +0200

    TextTypes added

---
+++ main.sh
@@ -0,0 +1,2 @@
+chmod +x main.sh
+python3 src/main.py
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    print("hello world")
+    my_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(my_node)
+
+main()
