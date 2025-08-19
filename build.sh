5580575395e2ad4931a87d6b52714890f79cba18
RadSan7 <radek.san7@icloud.com>
2025-08-19 18:29:20 +0200

    d

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # build.sh — budowanie strony na produkcję
 
-REPO_NAME="python_static"
 
 # przejdź do katalogu nadrzędnego (gdzie python_static jest pakietem)
 cd "$(dirname "$0")/.."
 
-python3 -m python_static.src.main "/$REPO_NAME/"
+python3 -m python_static.src.main "/python_static/"
